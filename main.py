28108eecf66b8b54d107130dedbcd9d8b6fbc3e2
alokpandey098 <kalok0575@gmail.com>
2024-02-03 15:39:51 +0530

    Data Ingestion Completed

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 from cnnClassifier import logger
-logger.info("Create Log For Potatoes Disease classifier Project .")
+from cnnClassifier.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
